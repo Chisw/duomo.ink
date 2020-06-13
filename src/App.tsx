47e6f3517a0bcc9dfe0c6f867e35c397621ca3cb
Chisw <chisw@qq.com>
2020-06-13 12:42:15 +0800

    eject and add tailwind, evergreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import logo from './images/logo.png'
 import './css/index.css'
+import { Button } from 'evergreen-ui'
 
 function App() {
   return (
     <div>
       <img src={logo} className="w-12" alt="logo" />
+      <Button>a</Button>
     </div>
   )
 }
