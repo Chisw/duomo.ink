bbada18aa34cbf43db383dbf6611357b365844a7
Chisw <chisw@qq.com>
2020-06-13 12:35:22 +0800

    init CRA

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './images/logo.png'
+import './css/index.css'
+
+function App() {
+  return (
+    <div>
+      <img src={logo} className="w-12" alt="logo" />
+    </div>
+  )
+}
+
+export default App
